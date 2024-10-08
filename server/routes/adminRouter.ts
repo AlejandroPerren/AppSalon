9ff7f44185ad8b9afcfd190b17f4a9d18b3b2aa8
Alejandro <aletomasperren@gmail.com>
2024-10-08 11:21:08 -0300

    Routes

--- server/routes/adminRouter.ts
+++ server/routes/adminRouter.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { tokenMiddleware } from '../middlewares/tokenMiddleware';
-import { CitasAdminController , deleteCita } from '../controllers/admin/citasController';
+import { CitasAdminController } from '../controllers/admin/citasController';
 
 const router = express.Router(); 
 
 router.get('/citas',tokenMiddleware, CitasAdminController);
-router.post('/modcitas',tokenMiddleware, deleteCita);
+router.post('/modcitas',tokenMiddleware, );
 
 
 
